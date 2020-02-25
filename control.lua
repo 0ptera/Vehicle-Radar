e2b4c6072889394e8021c417ab94e71d4adfff0a
Optera <yousei.kyoko+github@gmail.com>
2020-02-25 19:21:09 +0100

    Version: 0.6.1 Date: 2020-02-25   Features:     - reduce overhead using on_sector_scanned event filter

--- control.lua
+++ control.lua
@@ -5,44 +5,40 @@ require("config")
 function init_force(force)
   global.trains[force.name] = {}
   global.vehicles[force.name] = {}
-  local train_count = 0
-  local car_count = 0
   for _,surface in pairs(game.surfaces) do
     local locomotives = surface.find_entities_filtered{force = force, type = "locomotive"}
     for _,loco in pairs(locomotives) do
       global.trains[force.name][loco.unit_number] = loco
-      train_count = train_count + 1
     end
 
     local cars = surface.find_entities_filtered{force = force, type = "car"}
     for _,car in pairs(cars) do
       global.vehicles[force.name][car.unit_number] = car
-      car_count = car_count +1
     end
 
   end
-  log("[VT] Found "..train_count.." trains for force "..force.name)
-  log("[VT] Found "..car_count.." vehicles for force "..force.name)
+  log("[VT] Found "..table_size(global.trains[force.name]).." trains for force "..force.name)
+  log("[VT] Found "..table_size(global.vehicles[force.name]).." vehicles for force "..force.name)
 end
 
 function init()
   global.trains = {}
   global.vehicles = {}
-	for _,force in pairs(game.forces) do
+  for _,force in pairs(game.forces) do
     init_force(force)
-	end
+  end
 end
 
 script.on_init(function()
-	init()
+  init()
 end)
 
 script.on_configuration_changed(function(event)
-	init()
+  init()
 end)
 
 script.on_event(defines.events.on_force_created, function(event)
-	init_force(event.force)
+  init_force(event.force)
 end)
 
 script.on_event(defines.events.on_forces_merging, function(event)
@@ -71,51 +67,54 @@ script.on_event(defines.events.on_train_created, function(event)
 end)
 
 script.on_event(defines.events.on_built_entity, function(event)
-	local entity = event.created_entity
-	if entity.type == "locomotive" then
+  local entity = event.created_entity
+  if entity.type == "locomotive" then
     global.trains[entity.force.name][entity.unit_number] = entity
-	end
-	if entity.type == "car" then
+  end
+  if entity.type == "car" then
     global.vehicles[entity.force.name][entity.unit_number] = entity
-	end
+  end
 end)
 
-script.on_event(defines.events.on_sector_scanned, function(event)
-	if event.radar.name == "train-tracker" then
-		local force = event.radar.force
-    for uid, loco in pairs(global.trains[force.name]) do
-    	if loco.valid then
-				local x1 = loco.position.x
-				local y1 = loco.position.y
-				local x2 = x1 + loco.train.speed * train_tracker.precognotion * math.sin(2*math.pi * loco.orientation)
-				local y2 = y1 - loco.train.speed * train_tracker.precognotion * math.cos(2*math.pi * loco.orientation)
+script.on_event(defines.events.on_sector_scanned,
+  function(event)
+    if event.radar.name == "train-tracker" then
+      local force = event.radar.force
+      for uid, loco in pairs(global.trains[force.name]) do
+        if loco.valid then
+          local x1 = loco.position.x
+          local y1 = loco.position.y
+          local x2 = x1 + loco.train.speed * train_tracker.precognotion * math.sin(2*math.pi * loco.orientation)
+          local y2 = y1 - loco.train.speed * train_tracker.precognotion * math.cos(2*math.pi * loco.orientation)
 
-				if x2 < x1 then
-					local x = x1
-					x1 = x2
-					x2 = x
-				end
-				if y2 < y1 then
-					local y = y1
-					y1 = y2
-					y2 = y
-				end
-				force.chart(event.radar.surface,{{x1-train_tracker.scan_radius, y1-train_tracker.scan_radius}, {x2+train_tracker.scan_radius, y2+train_tracker.scan_radius}})
-			else
-				global.trains[force.name][uid] = nil
-			end
-		end
-	elseif event.radar.name == "vehicular-tracker" then
-		local force = event.radar.force
-    for uid, vehicle in pairs(global.vehicles[force.name]) do
-			if vehicle.valid then
-				local x1 = vehicle.position.x
-				local y1 = vehicle.position.y
+          if x2 < x1 then
+            local x = x1
+            x1 = x2
+            x2 = x
+          end
+          if y2 < y1 then
+            local y = y1
+            y1 = y2
+            y2 = y
+          end
+          force.chart(event.radar.surface,{{x1-train_tracker.scan_radius, y1-train_tracker.scan_radius}, {x2+train_tracker.scan_radius, y2+train_tracker.scan_radius}})
+        else
+          global.trains[force.name][uid] = nil
+        end
+      end
+    elseif event.radar.name == "vehicular-tracker" then
+      local force = event.radar.force
+      for uid, vehicle in pairs(global.vehicles[force.name]) do
+        if vehicle.valid then
+          local x1 = vehicle.position.x
+          local y1 = vehicle.position.y
 
-				force.chart(event.radar.surface,{{x1-vehicle_tracker.scan_radius, y1-vehicle_tracker.scan_radius}, {x1+vehicle_tracker.scan_radius, y1+vehicle_tracker.scan_radius}})
-			else
-        global.vehicles[force.name][uid] = nil
-			end
-		end
-	end
-end)
+          force.chart(event.radar.surface,{{x1-vehicle_tracker.scan_radius, y1-vehicle_tracker.scan_radius}, {x1+vehicle_tracker.scan_radius, y1+vehicle_tracker.scan_radius}})
+        else
+          global.vehicles[force.name][uid] = nil
+        end
+      end
+    end
+  end,
+  { {filter="name", name = "train-tracker"}, {filter="name", name = "vehicular-tracker"} }
+)
